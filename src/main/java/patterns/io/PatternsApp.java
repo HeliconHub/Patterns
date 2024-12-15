c2dfa59ed79070493ec15729f315ef8b6a53d221
Sujay Samanta <sujay.samanta@hashicorp.com>
2024-12-15 21:23:31 +0530

    added gradle based application setup

---
+++ src/main/java/patterns/io/PatternsApp.java
@@ -0,0 +1,13 @@
+package patterns.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternsApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatternsApp.class, args);
+	}
+
+}
---
+++ src/test/java/patterns/io/PatternsAppTests.java
@@ -0,0 +1,13 @@
+package patterns.io;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternsAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
